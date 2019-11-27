3ba1aef2d00752a12d88102009da99f2b4bf3f8f
AangJnr <cion.aang@gmail.com>
2019-11-27 13:12:27 +0000

    Database updates on 'monitorings and added configurable urls' table

--- app/src/main/java/org/grameen/fdp/kasapin/data/db/entity/Monitoring.java
+++ app/src/main/java/org/grameen/fdp/kasapin/data/db/entity/Monitoring.java
@@ -30,62 +30,44 @@ public class Monitoring {
     String plotExternalId;
     public Monitoring() {
     }
-
-
     public void setExternalId(String externalId) {
         this.externalId = externalId;
     }
-
     public String getExternalId() {
         return externalId;
     }
-
     public String getYear() {
         return year;
     }
-
     public void setYear(String year) {
         this.year = year;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public int getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getJson() {
         return json;
     }
-
     public void setJson(String json) {
         this.json = json;
     }
-
     public String getPlotExternalId() {
         return plotExternalId;
     }
-
     public void setPlotExternalId(String plotExternalId) {
         this.plotExternalId = plotExternalId;
     }
-
-
     @Ignore
     public JSONObject getMonitoringAOJsonData() throws JSONException {
         return new JSONObject(json);
     }
-
-
-
 }
