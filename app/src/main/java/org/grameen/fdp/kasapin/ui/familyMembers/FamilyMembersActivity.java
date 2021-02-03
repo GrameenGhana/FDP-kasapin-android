914851d4609d5ede850073c539d96596aafc7520
myxroft <josiahbuhayan@gmail.com>
2021-02-03 11:42:27 +0800

    Readjusted the padding to deal with validation display issues.

--- app/src/main/java/org/grameen/fdp/kasapin/ui/familyMembers/FamilyMembersActivity.java
+++ app/src/main/java/org/grameen/fdp/kasapin/ui/familyMembers/FamilyMembersActivity.java
@@ -295,7 +295,7 @@ public class FamilyMembersActivity extends BaseActivity implements FamilyMembers
         materialEditText.setTag(tag);
         materialEditText.setTextSize(15f);
         materialEditText.setTextSize(15f);
-        materialEditText.setPadding(10,20,10,80);
+        materialEditText.setPadding(10,20,10,100);
 
         materialEditText.addTextChangedListener(new TextWatcher() {
             @Override
